5c631629dc2700e12356767fee0c185b68c88069
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-09 23:03:31 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const main = document.querySelector("main");
+
+main.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const { name } = e.target.dataset;
+    if (name === "add-btn") {
+      const todoInput = main.querySelector('[data-name="todo-input"]');
+      if (todoInput.value.trim() !== "") {
+        const value = todoInput.value;
+        const template = `
+        <li class="listitem" draggable="true" data-id="${Date.now()}">
+          <p>${value}</p>
+          <button class="btnsub" data-name="remove-btn">X</button>
+        </li>
+        `;
+        const todosList = main.querySelector('[data-name="todos-list"]');
+        todosList.insertAdjacentHTML("beforeend", template);
+        todoInput.value = "";
+      }
+    } else if (name === "remove-btn") {
+      e.target.parentElement.remove();
+    }
+  }
+});
+
+main.addEventListener("dragenter", (e) => {
+  if (e.target.classList.contains("list")) {
+    e.target.classList.add("drop");
+  }
+});
+
+main.addEventListener("dragleave", (e) => {
+  if (e.target.classList.contains("drop")) {
+    e.target.classList.remove("drop");
+  }
+});
+
+main.addEventListener("dragstart", (e) => {
+  if (e.target.classList.contains("listitem")) {
+    e.dataTransfer.setData("text/plain", e.target.dataset.id);
+  }
+});
+
+let elemBelow = "";
+
+main.addEventListener("dragover", (e) => {
+  e.preventDefault();
+
+  elemBelow = e.target;
+});
+
+main.addEventListener("drop", (e) => {
+  const todo = main.querySelector(
+    `[data-id="${e.dataTransfer.getData("text/plain")}"]`
+  );
+
+  if (elemBelow === todo) {
+    return;
+  }
+
+  if (elemBelow.tagName === "P" || elemBelow.tagName === "BUTTON") {
+    elemBelow = elemBelow.parentElement;
+  }
+
+  if (elemBelow.classList.contains("listitem")) {
+    const center =
+      elemBelow.getBoundingClientRect().y +
+      elemBelow.getBoundingClientRect().height / 2;
+
+    if (e.clientY > center) {
+      if (elemBelow.nextElementSibling !== null) {
+        elemBelow = elemBelow.nextElementSibling;
+      } else {
+        return;
+      }
+    }
+
+    elemBelow.parentElement.insertBefore(todo, elemBelow);
+    todo.className = elemBelow.className;
+  }
+
+  if (e.target.classList.contains("list")) {
+    e.target.append(todo);
+
+    if (e.target.classList.contains("drop")) {
+      e.target.classList.remove("drop");
+    }
+
+    const { name } = e.target.dataset;
+
+    if (name === "completed-list") {
+      if (todo.classList.contains("in-progress")) {
+        todo.classList.remove("in-progress");
+      }
+      todo.classList.add("completed");
+    } 
+  }
+});
